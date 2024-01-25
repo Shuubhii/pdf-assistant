2c22a28d39259e19bec3f3d6bd832af3ab5c7b83
Shuubhii <126624347+Shuubhii@users.noreply.github.com>
2024-01-26 01:11:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
 
-# Commented out IPython magic to ensure Python compatibility.
-# %%writefile app.py
+
+#Commented out IPython magic to ensure Python compatibility.
+
+#%%writefile app.py
 import streamlit as st
 import time
 import os
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
-from detectron2.config import get_cfg
+#from detectron2.config import get_cfg
 
 os.environ["OPENAI_API_KEY"] = st.secrets["API-KEY"]
 
 
 def pdf_model(pdf_path):
-  cfg = get_cfg()
-  cfg.MODEL.DEVICE = 'cpu'
+#  cfg = get_cfg()
+#  cfg.MODEL.DEVICE = 'cpu'
   loaders = [UnstructuredPDFLoader(pdf_path)]
   index = VectorstoreIndexCreator().from_loaders(loaders)
   return index
@@ -23,10 +25,18 @@ text_folder = '/content/48lawsofpower.pdf'
 
 
 
-st.title("PDF-Assistant")
+st.sidebar.title("PDF-Assistant")
+st.sidebar.write("# Welcome to the time machine demo 👋")
+
+st.sidebar.write("""
+            Please go ahead and:
+             - Upoad some data
+             - Start interacting
+             """)
+
 
  # upload file
-file = st.file_uploader("Upload your PDF to start Interacting", type="pdf")
+file = st.sidebar.file_uploader("Upload your PDF to start Interacting", type="pdf")
 
 def save_temp_file(uploaded_file):
     temp_dir = "temp_files"
@@ -40,7 +50,7 @@ def save_temp_file(uploaded_file):
 path=""
 index=""
 if file:
-  st.success(f"File uploaded: {file.name}")
+  st.sidebar.success(f"File uploaded: {file.name}")
   path=save_temp_file(file)
   index=pdf_model(path)
 
@@ -80,8 +90,11 @@ if prompt:=st.chat_input("Ask questions?"):
 # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
-#to get the password which is to be used in the localtunnel to access streamlit
-#! wget -q -O - https://loca.lt/mytunnelpassword
 
-#!streamlit run app.py &>/content/logs.txt &
-#!npx localtunnel --port 8501
+
+
+#to get the password which is to be used in the localtunnel to access streamlit
+# ! wget -q -O - https://loca.lt/mytunnelpassword
+
+# !streamlit run app.py &>/content/logs.txt &
+# !npx localtunnel --port 8501
